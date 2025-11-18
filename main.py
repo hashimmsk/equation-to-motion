a8176b10384b51dff4b1cf1b80d6253cfedff80f
hashi <hashimshahzad998@gmail.com>
2025-11-18 00:14:37 -0500

    improved user experience

--- main.py
+++ main.py
@@ -6,34 +6,25 @@ complies with the 15-112 term project pre-MVP restrictions.
 """
 
 from types import SimpleNamespace
-
-from cmu_graphics import app, runApp  # type: ignore[attr-defined]
-
+from cmu_graphics import app, runApp
 from mvp import controller
-
 
 def onAppStart(app):
     controller.app_started(app)
-
 
 def onKeyPress(app, key):
     event = SimpleNamespace(key=key)
     controller.key_pressed(app, event)
 
-
 def onMousePress(app, mouseX, mouseY):
     event = SimpleNamespace(x=mouseX, y=mouseY)
     controller.mouse_pressed(app, event)
 
-
 def onStep(app):
     controller.timer_fired(app)
-
 
 def redrawAll(app):
     controller.redraw_all(app)
 
-
 if __name__ == "__main__":
     runApp(width=1180, height=720)
-
